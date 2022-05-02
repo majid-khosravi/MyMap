2a7fa403a0470982d2cdf6cbfdf896d54cffcbd7
Majid <majid.khosravi89@gmail.com>
2022-05-02 19:19:08 +0430

    I've wrote document of some classes

--- common/src/main/java/ir/majidkhosravi/common/di/CoreModule.kt
+++ common/src/main/java/ir/majidkhosravi/common/di/CoreModule.kt
@@ -7,6 +7,12 @@ import dagger.hilt.components.SingletonComponent
 import ir.majidkhosravi.common.utils.GlobalDispatcher
 import kotlinx.coroutines.Dispatchers
 
+/**
+ * In this module I've implemented some common provider,
+ *  for instance I've defined the {@link GlobalDispatcher} provider to use in other modules
+ */
+
+
 @Module
 @InstallIn(SingletonComponent::class)
 class CoreModule {
--- common/src/main/java/ir/majidkhosravi/common/utils/GlobalDispatcher.kt
+++ common/src/main/java/ir/majidkhosravi/common/utils/GlobalDispatcher.kt
@@ -2,6 +2,12 @@ package ir.majidkhosravi.common.utils
 
 import kotlinx.coroutines.CoroutineDispatcher
 
+
+/**
+ * GlobalDispatcher is a common utils to create and keep instance of {@link CoroutineDispatcher} to use in other modules
+ */
+
+
 data class GlobalDispatcher(
     val main: CoroutineDispatcher,
     val io: CoroutineDispatcher,
